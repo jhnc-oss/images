b950ee0e2803aefcdb5b536d721fe848331bb333
offa <bm-dev@yandex.com>
2024-06-11 17:17:26 +0200

    Fail run-clang-format on error; add --git-diff to check for a formatting diff

--- clang-format/run-clang-format.sh
+++ clang-format/run-clang-format.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+set -e
+
 [[ -f .clang-format-ignore ]] && IGNORE_OPTS=("--ignore-file" ".clang-format-ignore")
 fd -e c -e cpp -e cxx -e cc -e h -e hpp "${IGNORE_OPTS[@]}" -x clang-format -i
+
+if [[ "$1" == "--git-diff" ]]; then
+    /usr/bin/git diff --exit-code HEAD
+fi
